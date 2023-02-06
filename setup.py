c686490d325db186d421c8f76c370e6e1de99466
sumanabasu <sumana.basu21@gmail.com>
2023-02-05 20:36:43 -0500

    paepomdp installer for the glucose control task

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="paepomdp",
+    version="0.1",
+    packages=['paepomdp'],
+    install_requires=['gym==0.9.4'],
+    include_package_data=True
+)
+
+
