dfef954091aa5a2a2f623c6e00c7e3c58d553ff3
sumanabasu <sumana.basu21@gmail.com>
2023-10-30 12:16:09 -0400

    minor changes

--- paepomdp/diabetes/trainers/effdqn_diabetes_trainer.py
+++ paepomdp/diabetes/trainers/effdqn_diabetes_trainer.py
@@ -3,20 +3,23 @@ import math
 import numpy as np
 import os
 import random
+import time
 import torch
-import argparse
+import torch.nn.functional as F
 from collections import defaultdict
 from datetime import datetime
-from paepomdp.algos.DQN import ReplayMemory, DQN
-from paepomdp.algos.networks import EFFDQN_Diabetes_Real_Net
-from paepomdp.diabetes.helpers.utils import register_single_patient_env, DiscretizeActionWrapper, \
+from torch.utils.tensorboard import SummaryWriter
+from code.algos.DQN import ReplayMemory, DQN
+from code.algos.networks import EFFDQN_Net, EFFDQN_Diabetes_Net
+from code.algos.utils import savePickle, PostionWrapper, augmented_state, effective_action_vector
+from code.diabetes.helpers.utils import register_single_patient_env, DiscretizeActionWrapper, \
 	save_learning_metrics
 
 def train(env,
 		  dir_,
-		  action_size,
 		  state_size,
 		  n_actions,
+		  n_hidden,
 		  state_embedding_size,
 		  action_embedding_size,
 		  M_episodes,
@@ -32,24 +35,24 @@ def train(env,
 		  TARGET_UPDATE,
 		  seed,
 		  hyperglycemic_BG,
-		  hypoglycemic_BG,
-		  n_hidden):
+		  hypoglycemic_BG):
 	device = torch.device ("cuda" if torch.cuda.is_available () else "cpu")
-
+	
 	metrics = defaultdict (list)
 	
 	# Set seeds
 	seed = seed
 	env.seed (seed)
+	# env.action_space.seed (seed)
 	torch.manual_seed (seed)
 	np.random.seed (seed)
 	random.seed(seed)
 	
 	# Initialize Replay Buffer
-	replay_buffer = ReplayMemory (replay_buffer_size, state_size+action_size, device)
-	policy_net = EFFDQN_Diabetes_Real_Net(n_actions, action_size,
-										  state_size, action_embedding_size,
-										  state_embedding_size, n_hidden).to (device)
+	replay_buffer = ReplayMemory (replay_buffer_size, state_size+n_actions, device)
+	policy_net = EFFDQN_Diabetes_Net(n_actions, state_size,
+							 action_embedding_size,
+							 state_embedding_size, n_hidden).to (device)
 	
 	# Initialize DQN algorithm
 	policy = DQN(n_actions,
@@ -72,20 +75,23 @@ def train(env,
 		done = False
 		current_return = 0
 		state = env.reset ()
-		eff_action = env.min_action
-		aug_state = torch.cat((torch.tensor([state[0]]).float().view (1, -1).to(device),
-							   torch.tensor([eff_action]).float().view (1, -1).to(device)), dim=-1)
+		eff_action = F.one_hot (torch.tensor (env.min_action), n_actions).float ()
+		aug_state = augmented_state(torch.tensor(state).float().view (1, -1).to(device),
+									eff_action.view (1, -1).to(device))
+		
 		
 		while done == False:
-			action = policy.select_action(aug_state, eps)
+			if state[0] < 150:
+				action = 0	#np.array([0])
+			else:
+				action = policy.select_action(aug_state, eps)
 			next_state, reward, done, info = env.step(np.array([action]))
 			done_bool = float (done)
 			current_return += reward
 			
-			next_eff_action = lambdaa * eff_action + action
-			
-			next_aug_state = torch.cat((torch.tensor([next_state[0]]).float().view (1, -1).to(device),
-										torch.tensor([next_eff_action]).float().view (1, -1).to(device)), dim=-1)
+			next_eff_action = effective_action_vector(lambdaa, n_actions, action, eff_action)
+			next_aug_state = augmented_state (torch.tensor (next_state).float ().view (1, -1).to(device),
+										 next_eff_action.view (1, -1).to (device))
 			
 			replay_buffer.push (aug_state,
 								torch.LongTensor ([ action ]),
@@ -116,7 +122,6 @@ def train(env,
 		metrics[ 'CGM_hist' ].extend (env.env.env.CGM_hist[:-1])
 		metrics[ 'insulin_hist' ].extend(np.concatenate(env.env.env.insulin_hist).ravel().tolist())
 		metrics[ 'CHO_hist' ].extend (env.env.env.CHO_hist)
-		metrics[ 'mortality' ].append(env.env.env.BG_hist[-1])
 		
 		
 		if i_episode % 1000 == 0:
@@ -124,49 +129,50 @@ def train(env,
 			
 		if i_episode % checkpoint_freq == 0:
 			save_learning_metrics (dir_, **metrics)
-			policy.save (dir_)
 	
 	save_learning_metrics (dir_, **metrics)
-	policy.save (dir_)
-	
+	# policy.save (dir_)
 	
 	print ('Done!')
 	
 	env.close ()
-	
-def run(args):
-	EXPERIMENTS = "../../../Experiments/EffDQN/"
-	print('Training Effective DQN for patient: ', args.patient_name)
 
-	env_id = register_single_patient_env (args.patient_name,
-										  reward_fun=args.reward,
-										  seed=args.seed,
+
+if __name__ == '__main__':
+	EXPERIMENTS = "../experiments/"
+	
+	patient_name = 'adult#009'
+	reward = 'zone_reward'
+	seed = 10
+	
+	env_id = register_single_patient_env (patient_name,
+										  reward_fun=reward,
+										  seed=seed,
 										  version='-v0')
 	env = gym.make (env_id)
-	env = DiscretizeActionWrapper (env, low=0, high=5, n_bins=6)
-	
+	env = DiscretizeActionWrapper (env)
+
 	kwargs = {
-			# Fixed
-			'state_size': env.observation_space.shape[ 0 ],
-			'action_size' : env.action_space.shape[0],
-			'n_actions': env.num_actions,
-			'state_embedding_size': 16,
-			'action_embedding_size': 16,
-			'M_episodes': 10000,
-			'replay_buffer_size': 1000000,
-			'batch_size': 512,
-			'eps_start': 0.9,
-			'eps_end': 0.05,
-			'gamma': 0.999,
-			'lambdaa': 0.95,
-			'learning_rate': 0.001,
-			'eps_decay': 500,
-			'EXPLORE': 1000,
-			'TARGET_UPDATE': 1,
-			'seed': args.seed,
-			'hyperglycemic_BG' : 150,
-			'hypoglycemic_BG' : 100,
-			'n_hidden' : 256
+		# Fixed
+		'state_size': env.observation_space.shape[ 0 ],
+		'n_actions': env.num_actions,
+		'state_embedding_size': 256,
+		'action_embedding_size': 256,
+		'M_episodes': 1000,
+		'replay_buffer_size': 1000000,
+		'batch_size': 4,
+		'eps_start': 0.9,
+		'eps_end': 0.05,
+		'gamma': 0.999,
+		'lambdaa': 0.7,
+		'learning_rate': 0.0005,
+		'eps_decay': 100,
+		'EXPLORE': 10,
+		'TARGET_UPDATE': 1,
+		'seed': 1,
+		'hyperglycemic_BG' : 150,
+		'hypoglycemic_BG' : 100,
+		'n_hidden' : 256
 	}
 	
 	expt_id = datetime.now ().strftime ('%Y-%m-%d %H:%M:%S')
@@ -177,15 +183,5 @@ def run(args):
 	dir_ = EXPERIMENTS + expt_id
 	os.makedirs (dir_)
 	print (f"Created {dir_}")
-	
-	train (env, dir_,  **kwargs)
-	
-if __name__ == '__main__':
-	parser = argparse.ArgumentParser (description="EffDQN argument parser")
-	parser.add_argument ("--patient_name", type=str, default='adult#009', help="Name of the patient")
-	parser.add_argument ("--reward", type=str, default='zone_reward', help="Reward type (default: zone_reward)")
-	parser.add_argument ("--seed", type=int, default=3, help="Seed value (default: 10)")
-		
-	args = parser.parse_args ()
-		
-	run(args)
+
+	train(env, dir_, **kwargs)
